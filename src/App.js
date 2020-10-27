30903cc36f9900f765acf96df88680cb9369980a
Ze Yen Chai <zeyenchai@gmail.com>
2020-10-28 01:04:21 +0800

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -31,13 +31,12 @@ class App extends React.Component {
               ...snapShot.data()
             }
           })
-          console.log(this.state)
         })
       }
 
-      // else {
-      this.setState({ currentUser: userAuth })
-      // }
+      else {
+        this.setState({ currentUser: userAuth })
+      }
     })
   }
 
